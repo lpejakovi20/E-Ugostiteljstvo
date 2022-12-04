46d7bc2ec0fb1bfc71711f430394c5915e7672a3
MatejRitosa <ritossamatej@gmail.com>
2022-12-04 21:58:14 +0100

    Dizajn formi za funkcionalnosti F01 i F06 #3 #8

---
+++ Software/E-ugostiteljstvo/E-ugostiteljstvo/FrmKreirajNarudžbenicu.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace E_ugostiteljstvo
+{
+    partial class FrmKreirajNarudžbenicu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvNarudžbenica = new System.Windows.Forms.DataGridView();
+            this.dgvNamirnice = new System.Windows.Forms.DataGridView();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.btnPopuni = new System.Windows.Forms.Button();
+            this.btnTraži = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.txtTraži = new System.Windows.Forms.TextBox();
+            this.btnMakni = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNarudžbenica)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNamirnice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvNarudžbenica
+            // 
+            this.dgvNarudžbenica.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNarudžbenica.Location = new System.Drawing.Point(63, 90);
+            this.dgvNarudžbenica.Name = "dgvNarudžbenica";
+            this.dgvNarudžbenica.RowHeadersWidth = 51;
+            this.dgvNarudžbenica.RowTemplate.Height = 24;
+            this.dgvNarudžbenica.Size = new System.Drawing.Size(374, 352);
+            this.dgvNarudžbenica.TabIndex = 0;
+            // 
+            // dgvNamirnice
+            // 
+            this.dgvNamirnice.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNamirnice.Location = new System.Drawing.Point(541, 90);
+            this.dgvNamirnice.Name = "dgvNamirnice";
+            this.dgvNamirnice.RowHeadersWidth = 51;
+            this.dgvNamirnice.RowTemplate.Height = 24;
+            this.dgvNamirnice.Size = new System.Drawing.Size(375, 352);
+            this.dgvNamirnice.TabIndex = 1;
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(841, 475);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(75, 45);
+            this.btnSpremi.TabIndex = 2;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            // 
+            // btnPopuni
+            // 
+            this.btnPopuni.Location = new System.Drawing.Point(63, 475);
+            this.btnPopuni.Name = "btnPopuni";
+            this.btnPopuni.Size = new System.Drawing.Size(88, 45);
+            this.btnPopuni.TabIndex = 3;
+            this.btnPopuni.Text = "Popuni automatski";
+            this.btnPopuni.UseVisualStyleBackColor = true;
+            // 
+            // btnTraži
+            // 
+            this.btnTraži.Location = new System.Drawing.Point(711, 42);
+            this.btnTraži.Name = "btnTraži";
+            this.btnTraži.Size = new System.Drawing.Size(75, 23);
+            this.btnTraži.TabIndex = 4;
+            this.btnTraži.Text = "Traži";
+            this.btnTraži.UseVisualStyleBackColor = true;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(841, 36);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(75, 35);
+            this.btnDodaj.TabIndex = 5;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // txtTraži
+            // 
+            this.txtTraži.Location = new System.Drawing.Point(553, 43);
+            this.txtTraži.Name = "txtTraži";
+            this.txtTraži.Size = new System.Drawing.Size(137, 22);
+            this.txtTraži.TabIndex = 6;
+            // 
+            // btnMakni
+            // 
+            this.btnMakni.Location = new System.Drawing.Point(63, 36);
+            this.btnMakni.Name = "btnMakni";
+            this.btnMakni.Size = new System.Drawing.Size(75, 35);
+            this.btnMakni.TabIndex = 7;
+            this.btnMakni.Text = "Makni";
+            this.btnMakni.UseVisualStyleBackColor = true;
+            // 
+            // FrmKreirajNarudžbenicu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 549);
+            this.Controls.Add(this.btnMakni);
+            this.Controls.Add(this.txtTraži);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.btnTraži);
+            this.Controls.Add(this.btnPopuni);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.dgvNamirnice);
+            this.Controls.Add(this.dgvNarudžbenica);
+            this.Name = "FrmKreirajNarudžbenicu";
+            this.Text = "FrmKreirajNarudžbenicu";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNarudžbenica)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNamirnice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvNarudžbenica;
+        private System.Windows.Forms.DataGridView dgvNamirnice;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Button btnPopuni;
+        private System.Windows.Forms.Button btnTraži;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.TextBox txtTraži;
+        private System.Windows.Forms.Button btnMakni;
+    }
+}
---
+++ Software/E-ugostiteljstvo/E-ugostiteljstvo/FrmRegistracija.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace E_ugostiteljstvo
+{
+    partial class FrmRegistracija
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbSlika = new System.Windows.Forms.PictureBox();
+            this.btnSlikaj = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.cmbRadnoMjesto = new System.Windows.Forms.ComboBox();
+            this.btnRegistriraj = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbSlika)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbSlika
+            // 
+            this.pcbSlika.Location = new System.Drawing.Point(75, 41);
+            this.pcbSlika.Name = "pcbSlika";
+            this.pcbSlika.Size = new System.Drawing.Size(219, 194);
+            this.pcbSlika.TabIndex = 0;
+            this.pcbSlika.TabStop = false;
+            // 
+            // btnSlikaj
+            // 
+            this.btnSlikaj.Location = new System.Drawing.Point(141, 257);
+            this.btnSlikaj.Name = "btnSlikaj";
+            this.btnSlikaj.Size = new System.Drawing.Size(75, 23);
+            this.btnSlikaj.TabIndex = 1;
+            this.btnSlikaj.Text = "Slikaj";
+            this.btnSlikaj.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "E-mail";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 335);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lozinka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 402);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Prezime";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Radno mjesto";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(102, 300);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(187, 22);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Location = new System.Drawing.Point(102, 329);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(187, 22);
+            this.txtLozinka.TabIndex = 9;
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(102, 361);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(187, 22);
+            this.txtIme.TabIndex = 10;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(102, 396);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(187, 22);
+            this.txtPrezime.TabIndex = 11;
+            // 
+            // cmbRadnoMjesto
+            // 
+            this.cmbRadnoMjesto.FormattingEnabled = true;
+            this.cmbRadnoMjesto.Location = new System.Drawing.Point(139, 438);
+            this.cmbRadnoMjesto.Name = "cmbRadnoMjesto";
+            this.cmbRadnoMjesto.Size = new System.Drawing.Size(150, 24);
+            this.cmbRadnoMjesto.TabIndex = 12;
+            // 
+            // btnRegistriraj
+            // 
+            this.btnRegistriraj.Location = new System.Drawing.Point(127, 495);
+            this.btnRegistriraj.Name = "btnRegistriraj";
+            this.btnRegistriraj.Size = new System.Drawing.Size(101, 42);
+            this.btnRegistriraj.TabIndex = 13;
+            this.btnRegistriraj.Text = "Registriraj se";
+            this.btnRegistriraj.UseVisualStyleBackColor = true;
+            // 
+            // FrmRegistracija
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 571);
+            this.Controls.Add(this.btnRegistriraj);
+            this.Controls.Add(this.cmbRadnoMjesto);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSlikaj);
+            this.Controls.Add(this.pcbSlika);
+            this.Name = "FrmRegistracija";
+            this.Text = "FrmRegistracija";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbSlika)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbSlika;
+        private System.Windows.Forms.Button btnSlikaj;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.ComboBox cmbRadnoMjesto;
+        private System.Windows.Forms.Button btnRegistriraj;
+    }
+}
