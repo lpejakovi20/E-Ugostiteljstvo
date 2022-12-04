e6d8f3d33383145d19a1c177838dd131239fa4f9
Nikola Parag <nparag20@foi.hr>
2022-12-04 20:58:38 +0100

    Kreiran dizajn izvjestaja namirnica kojima se blizi rok F07 #09

---
+++ Software/E-ugostiteljstvo/E-ugostiteljstvo/FrmIzvjestajBlizuRok.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace E_ugostiteljstvo
+{
+    partial class FrmIzvjestajBlizuRok
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dgvIzvjestaj = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIzvjestaj)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(529, 37);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(106, 40);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generiraj Izvještaj";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(25, 12);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(125, 34);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Natrag";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(41, 367);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(109, 55);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Spremi";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // dgvIzvjestaj
+            // 
+            this.dgvIzvjestaj.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvIzvjestaj.Location = new System.Drawing.Point(36, 95);
+            this.dgvIzvjestaj.Name = "dgvIzvjestaj";
+            this.dgvIzvjestaj.RowHeadersWidth = 51;
+            this.dgvIzvjestaj.RowTemplate.Height = 24;
+            this.dgvIzvjestaj.Size = new System.Drawing.Size(601, 266);
+            this.dgvIzvjestaj.TabIndex = 3;
+            // 
+            // FrmIzvjestajBlizuRok
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvIzvjestaj);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "FrmIzvjestajBlizuRok";
+            this.Text = "FrmIzvjestajBlizuRok";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIzvjestaj)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dgvIzvjestaj;
+    }
+}
